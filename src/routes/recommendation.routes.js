330186090e6f8e4d14651e5523b83843e1008b0c
pedromaironi <pedromaironi@gmail.com>
2023-08-23 14:22:02 -0700

    Recommendation based on interactions route added

--- src/routes/recommendation.routes.js
+++ src/routes/recommendation.routes.js
@@ -7,7 +7,10 @@ const recommendationController = require("../controllers/recommendation.controll
 router.route("/:id_usuario").get(async (req, res) => {
   const id_usuario = parseInt(req.params.id_usuario);
 
-  const pythonProcess = spawn("python", ["./python/main.py", id_usuario]);
+  const pythonProcess = spawn("python", [
+    "./python/BasedContentRecommendation.py",
+    id_usuario,
+  ]);
   let errors = "";
   let recommendations = [];
 
@@ -26,11 +29,47 @@ router.route("/:id_usuario").get(async (req, res) => {
         .filter((line) => line !== "")
         .map((line) => parseInt(line))
         .slice(0, 5);
-      // Fetch product details for recommended products
+
       const recommendedProducts =
         await recommendationController.fetchRecommendedProducts(
           id_usuario,
           recommendationNumbers
+        );
+      res.json(recommendedProducts);
+    } else {
+      console.error(`Python process finished with exit code ${code}`);
+      console.error("Python script errors:", errors);
+      res
+        .status(500)
+        .send("An error occurred while generating recommendations.");
+    }
+  });
+});
+
+router.route("/interactions/:id_usuario").get(async (req, res) => {
+  const id_usuario = parseInt(req.params.id_usuario);
+
+  const pythonProcess = spawn("python", [
+    "./python/ClicksContentRecommendation.py",
+    id_usuario,
+  ]);
+  let errors = "";
+  let recommendations = [];
+
+  pythonProcess.stdout.on("data", (data) => {
+    recommendations += data.toString();
+  });
+
+  pythonProcess.stderr.on("data", (data) => {
+    errors += data.toString();
+  });
+
+  await pythonProcess.on("close", async (code) => {
+    if (code === 0) {
+      const recommendedProducts =
+        await recommendationController.fetchRecommendedProducts(
+          id_usuario,
+          JSON.parse(recommendations.trim())
         );
       res.json(recommendedProducts);
     } else {
